dd2474d077d2c99163f6520980c63554990778af
davidsheh <davidsheh@davidsheh.lan>
2017-05-13 22:30:01 +0800

    [add] add VertexColored shader and Triangle.cs

---
+++ Assets/VertexColored/Triangle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class Triangle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		CreateTriangle();
+	}
+	
+	private void CreateTriangle()
+	{
+		Mesh mesh = new Mesh ();
+		Vector3[] vertex = new Vector3[3];
+		vertex [0] = new Vector3 (-1, 0, 0);
+		vertex [1] = new Vector3 (1, 0, 0);
+		vertex [2] = new Vector3 (0, 1, 0);
+
+		int[] triangles = new int[3]{ 0, 2, 1 };
+
+		Color[] colors = new Color[3]{ Color.red, Color.green, Color.blue };
+
+		//Vector3[] normals = new Vector3[3]{ Vector3.up, Vector3.up, Vector3.up };
+		//		Vector3[] normals = new Vector3[3]{ Vector3.forward, Vector3.forward, Vector3.forward };
+
+		mesh.vertices = vertex;
+		mesh.triangles = triangles;
+		mesh.colors = colors;
+		//mesh.normals = normals;
+		mesh.RecalculateNormals ();
+		GetComponent<MeshFilter> ().mesh = mesh;
+	}
+}
---
+++ Assets/VertexColored/Vertex Colored.shader
@@ -0,0 +1,19 @@
+﻿// 方法一
+Shader "Custom/VertexColor" {
+	Category {
+	     BindChannels { 
+	         Bind "Color", color 
+	         Bind "Vertex", vertex
+	     }
+	     SubShader { Pass { } }
+	 }
+ }
+
+// 方法二
+//Shader "Custom/Vertex Colored" {
+//     SubShader {
+//         Pass {
+//                 ColorMaterial AmbientAndDiffuse
+//         }
+//     } 
+//}
