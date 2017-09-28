2600ad0097d4dc53b8accf3bcd2e6b787f9dff32
Sheh伟伟 <davidsheh1015@gmail.com>
2017-09-28 19:14:04 +0800

    [add] add some effect

---
+++ Assets/SomeEffect/PostEffectScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PostEffectScript : MonoBehaviour {
+
+    public Material mat;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, mat);
+    }
+}
